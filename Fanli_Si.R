830bd341baca3bd84bf3a56ba8090344721c624f
Fanli Si <fsi4197@gmail.com>
2016-11-16 19:03:07 -0800

    updates

--- Fanli_Si.R
+++ Fanli_Si.R
@@ -1,5 +1,5 @@
-ggplot(subset(gapminder,continent != "Oceania"),
-       aes(x = year, y = pop, group = country, color = country)) +
-  geom_line(lwd = 1, show.legend = FALSE)  + facet_wrap(~ continent) +
-  scale_color_manual(values = country_colors) +
+ggplot(gapminder,
+       aes(x = year, y = lifeExp, group = continent, color = continent)) +
+  geom_point(lwd = 1, show.legend = FALSE)  +  geom_smooth(method="auto") + facet_wrap(~ continent)
+  scale_color_manual(values = continent_colors) +
   theme_bw() + theme(strip.text = element_text(size = rel(1.1)))
